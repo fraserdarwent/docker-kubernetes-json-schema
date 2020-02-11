d5a2ca16feed49160db74c3756def0f44a10eb28
Fraser Darwent <fraser@darwen.tf>
2020-02-11 10:52:35 +0000

    update release script

--- release-all.sh
+++ release-all.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 # Release each version
+echo "Releasing Docker images for all API versions with $(grep 'caddy-version=' makefile)"
 for version in $(cat versions.txt)
 do
   make release kubernetes-json-schema-version=${version}
